29ce1771ca2e8eb278274c78d45098f227407587
DvM17 <102309702+DvM17@users.noreply.github.com>
2022-04-22 15:51:03 +0900

    Update ninja_gram.sh

--- ninja_gram.sh
+++ ninja_gram.sh
@@ -20,15 +20,24 @@ apt install curl -y
 apt install nodejs -y
 apt install unzip -y
 apt install python -y
-pkg install -y python ndk-sysroot clang make \
+apt install -y python ndk-sysroot clang make \
     libjpeg-turbo
+apt install lxml
+apt install libxml2
+apt install libxslt
 unzip ninja_gram.zip
+pip install --upgrade pip
+pip install twine
+pip install selenium
+pip install colorama
 pip install -U instabot
 pip install instagram_private_api
 pip install git+https://git@github.com/ping/instagram_private_api.git@1.6.0
 python3 -c "$(curl https://gist.github.com/vsajip/4673395/raw/3420d9150cele9797dc8522fce7386d8643b02al/pyenv.py)" env-dir
 curl -O https://nodejs.org/dist/v14.15.4/SHASUMS256.txt.sig
-npm i instabot
+npm install instabot
+npm install instagram-followers
+npm install instagram-private-api
 echo ""
 echo "DON'T FORGET TO SUBSCRIBE PORMES17"
 echo ""
